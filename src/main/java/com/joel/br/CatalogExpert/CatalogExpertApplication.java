c230f127507f326c40bace3abd295953ab0969ac
joelsantos-js <joeltere9@gmail.com>
2024-01-24 12:32:47 -0300

    Initial commit

---
+++ src/main/java/com/joel/br/CatalogExpert/CatalogExpertApplication.java
@@ -0,0 +1,13 @@
+package com.joel.br.CatalogExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/br/CatalogExpert/CatalogExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.CatalogExpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
